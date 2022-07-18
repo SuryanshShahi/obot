6c76ec2c9b93d87868bad64e547902982e795258
SURYANSH SHAHI <70589762+SuryanshShahi@users.noreply.github.com>
2022-07-18 15:58:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
-// import Home from "./Home";
-import Test1 from "../obot/Techno";
+import Home from "./Home";
 import "./App.css";
 
 const App = () => {
   return (
     <section>
       <div className="app">
-        <Test1 />
+        <Home />
       </div>
     </section>
   );
